e3d548d72b3b91787ec58d4b4a95a8e53cf477f9
thomasfong <122613364+thomasfong@users.noreply.github.com>
2023-09-17 03:56:44 +0800

    Update getDates.js

---
+++ scripts/getDates.js
@@ -0,0 +1,6 @@
+var date = new Date();
+var year = date.getFullYear();
+document.getElementById('currentYear').innerHTML = year;
+
+let text = document.lastModified;
+document.getElementById("lastModified").innerHTML = text;
