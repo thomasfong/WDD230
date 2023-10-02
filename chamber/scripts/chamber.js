20fa330d54b6a55902c007834ceab2d1cc7f3000
thomasfong <thomasfong1020@gmail.com>
2023-10-02 21:30:53 +0800

    update discover page

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -24,3 +24,37 @@ document.getElementById('lastModified').innerHTML = `Last modified: ${oLastModif
 let d = new Date();
 let currentYear = d.getFullYear();
 document.getElementById('copyRight').innerHTML = `&copy ${currentYear} William Keel &#127482;&#127480`;
+
+
+/* discover.html-page*/
+
+<script>
+    let slideIndex = 1;
+    showSlides(slideIndex);
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+}
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+}
+
+    function showSlides(n) {
+        let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("demo");
+    let captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
+}
+</script>
