54eb48218cc7eab3de747f065c8e64d3592a0942
thomasfong <thomasfong1020@gmail.com>
2023-10-17 22:15:24 +0800

    update home pages

---
+++ chamber/images/images/learning/text.js
@@ -0,0 +1,36 @@
+//數字 
+let age = 28;
+console.log(age, typeof (age));
+
+//string 字串
+let language = 'English';
+let name = 'king'
+
+console.log(language + name)
+
+//Boolean
+
+let a = null
+let b;
+console.log(b)
+
+let hobbies = ['打', '寫', 10, true];
+console.log(hobbies)
+
+let person = {
+    name: 'Ho Yin',
+    age: 30,
+    goodHobbies: 'gogo'
+}
+console.log(person)
+
+
+
+
+
+
+
+
+
+
+
